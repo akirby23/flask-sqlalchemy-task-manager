4503ab5c8d05ea7ef1213af880d93bcd59d90204
akirby23 <>
2023-11-05 20:20:06 +0000

    Add functionality to create, read, update & delete categories and tasks

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(sidenav);
+
+    // Datepicker initialisation
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    // Select initialisation
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+
+    // Collapsible initialisation
+    let collapsibles = document.querySelectorAll(".collapsible");
+    M.Collapsible.init(collapsibles);
+});
+
+
+
+
+
+
+
